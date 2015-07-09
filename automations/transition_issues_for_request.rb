6eaecf0186fc8510c617efecad7e92731c16ab34
tolomaus <niek.bartholomeus@gmail.com>
2015-07-09 14:33:24 +0200

    Subtract 1000 from the received params["request_id"]

--- automations/transition_issues_for_request.rb
+++ automations/transition_issues_for_request.rb
@@ -2,7 +2,7 @@ brpm_rest_client = BrpmRestClient.new
 params = BrpmAuto.params
 
 BrpmAuto.log  "Getting the tickets that are linked to the request..."
-tickets = brpm_rest_client.get_tickets_by_request_id(params["request_id"])
+tickets = brpm_rest_client.get_tickets_by_request_id(params.rest_request_id)
 
 if tickets.count == 0
   BrpmAuto.log "This request has no tickets, nothing further to do."
@@ -11,7 +11,7 @@ end
 
 unless params["target_issue_status"]
   BrpmAuto.log  "Getting the stage of this request..."
-  request_with_details = brpm_rest_client.get_request_by_id(params["request_id"])
+  request_with_details = brpm_rest_client.get_request_by_id(params.rest_request_id)
 
   if request_with_details.has_key?("plan_member")
     stage_name = request_with_details["plan_member"]["stage"]["name"]
