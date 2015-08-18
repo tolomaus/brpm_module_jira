2238a85caa71a3e19430a40f8cd53ec597690c4c
tolomaus <niek.bartholomeus@gmail.com>
2015-08-18 10:37:33 +0200

    Fix - input param transition state is empty

--- automations/transition_issues_for_request.rb
+++ automations/transition_issues_for_request.rb
@@ -9,7 +9,7 @@ if tickets.count == 0
   return
 end
 
-unless params["target_issue_status"]
+if params["target_issue_status"].nil? or params["target_issue_status"].empty?
   BrpmAuto.log  "Getting the stage of this request..."
   request_with_details = brpm_rest_client.get_request_by_id(params.rest_request_id)
 
